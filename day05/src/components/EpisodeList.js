ed70e297c898b4cc2440693a11a7206720255ec6
Jason Horvath <jhorvath@medforce.net>
2021-12-14 19:24:29 -0500

    continued work on day 05

--- day05/src/components/EpisodeList.js
+++ day05/src/components/EpisodeList.js
@@ -1,12 +1,35 @@
+import { useState, useEffect } from 'react'
 import episodes from '../data/episodes.js'
 import Episode from './Episode.js'
 
 const EpisodeList = () => {
+  const [episodeState, setEpisodeState] = useState({})
+  const manageChecklist = (id) => {
+    // const updateObject = Object.assign({}, episodeState)
+
+  }
+
+  useEffect(() => {
+    const updateObject = {}
+    episodes.forEach((episode) => {
+      const { id } = {...episode}
+      // console.log(episodeState)
+      Object.assign(updateObject, {...episodeState})
+      updateObject[id] = false;
+      // console.log(updateObject)  
+      
+    })
+    
+    console.log(setEpisodeState({...updateObject}));
+    console.log(updateObject)
+    console.log(episodeState)
+  }, episodeState)
 
   return (
     <ul className="episodes">
       {episodes.map(episode => {
-        return <Episode key={episode.id} episode={episode} />
+        const { id } = {...episode}
+        return <Episode key={id} episode={episode} onClick={(e) => manageChecklist(id)} />
       })}
     </ul>
   )
