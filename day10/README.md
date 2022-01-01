f534c3c9abcdbabfc06f5672cc1971dd5c9bd9c1
Jason Horvath <jasonhorvath@jasons-mbp.myfiosgateway.com>
2022-01-01 18:48:13 -0500

    day10 complete

---
+++ day10/README.md
@@ -0,0 +1,3 @@
+# 11 - 4 Digit Code || Advent of JavaScript
+
+Written in vanilla Javascript. Default input events are prevented and each case of what keys are pressed and cursor advancement/focus are handled separately.
--- day10/app.js
+++ day10/app.js
@@ -1,37 +1,72 @@
 const inputs = [...document.querySelectorAll('.fields > input')]
-console.log(inputs)
+const verifyButton = document.querySelector('.wrapper > form > button')
 
-const validKey = (key) => {
-  console.log(key)
+const validInput = (key) => {
   return /[0-9]/.test(key)
 }
 
 const isLastInput = (key) => {
   return parseInt(key + 1) === parseInt(inputs.length)
 }
+const getCode = () => {
+  const values = []
+  inputs.forEach(input => values.push(input.value))
+  return values.join('');
+}
 
-inputs.forEach((input, key) => {
-  console.log(input)
-  input.addEventListener('keydown', (e) => {
-    e.preventDefault()
-    const el = e.target
-    if(e.key === 'Backspace') {
-      input.value = ''
-      if(key > 0) {
-        inputs[(key - 1)].focus()
+const runVerification = () => {
+  console.log('Verifying: ' + getCode())
+}
+
+const populateFromClipBoard = () => {
+  return navigator.clipboard.readText().then((pasted) => {
+    const populateValues = pasted.substring(0, 4).split('')
+    populateValues.forEach((value, index) => {
+      if(validInput(value)) {
+        inputs[index].value = value
+        console.log(value)
       }
-      return
+    })
+    runVerification()
+  }).catch(() => console.error('Clipboard error'))
+}
+
+const handleInput = (props) => {
+  const { e, input, key } = props
+  e.preventDefault()
+  if((e.metaKey === true || e.ctrlKey === true) && e.key.toLowerCase() === 'v') {
+    populateFromClipBoard()
+    return
+  }
+  
+  const el = e.target
+  if(e.key === 'Backspace') {
+    input.value = ''
+    if(key > 0) {
+      inputs[(key - 1)].focus()
     }
-    
-    console.log(e)
-    console.log(validKey(e.key))
-    if(validKey(e.key)) {
-      input.value = e.key
-    }
-    if(!isLastInput(key)) {
-      const nextInputKey = parseInt(key + 1)
-      el.blur()
-      inputs[nextInputKey].focus()
-    }
-  })
+    return
+  }
+
+  if(validInput(e.key)) {
+    input.value = e.key
+  } else {
+    return
+  }
+
+  if(!isLastInput(key)) {
+    const nextInputKey = parseInt(key + 1)
+    el.blur()
+    inputs[nextInputKey].focus()
+    return
+  } 
+  runVerification()
+}
+inputs.forEach((input, key) => {
+  input.addEventListener('keydown', (e) => handleInput({input, key, e}))
+})
+
+verifyButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  runVerification()
 })
